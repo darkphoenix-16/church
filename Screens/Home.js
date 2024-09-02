b170d5dc62d5009f09f70e40a1f44f18d5ca50a6
oyindamola adeonigbagbe <oyindamolatabitha@gmail.com>
2024-09-02 15:59:50 +0100

    commit

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
@@ -6,17 +6,55 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 export function Home() {
     return (
 
-        <SafeAreaView style={{flex:1}}>
+        <SafeAreaView style={{ flex: 1 }}>
+            <ScrollView>
             <View style={styles.container}>
                 <View style={{}}>
-                <Text style={{fontSize: 30, fontWeight:"bold", marginVertical:20}}>Welcome User</Text>
-                <TextInput style={{borderWidth: 1, borderRadius: 10, padding: 10}}/>
+                    <Text style={{ fontSize: 30, fontWeight: "condensedBold", marginVertical: 20, paddingHorizontal: 10 }}>Welcome User</Text>
+                    <TextInput style={{ borderWidth: 1, borderRadius: 10, padding: 10, marginVertical: 20, fontSize: 24}} placeholder='Search Events' />
                 </View>
-            </View>
 
-            <View>
-                
+                <View style={{ flexDirection: "row", justifyContent: "space-around", padding: 10, backgroundColor: "white" }}>
+                    <View>
+                        <Image source={{ uri: "https://i.pinimg.com/736x/23/a2/f3/23a2f39014f3db9981ba006a536deb3b.jpg" }} style={{ width: 190, height: 160, borderRadius: 10, opacity:0.6}} />
+                        <Text style={{ padding: 7, position: "absolute", top: 20, left: 55, color: "black", fontSize: 20, fontWeight: "bold" }}>Sermons</Text>
+                    </View>
+                    <View>
+                        <Image source={{ uri: "https://i.pinimg.com/736x/23/a2/f3/23a2f39014f3db9981ba006a536deb3b.jpg" }} style={{ width: 190, height: 160, borderRadius: 10, opacity:0.6 }} />
+                        <Text style={{ padding: 7, position: "absolute", top: 20, left: 35, color: "black", fontSize: 20, fontWeight: "bold" }}>Prayer request</Text>
+                    </View>
+                </View>
+                <View style={{ flexDirection: "row", justifyContent: "space-around", padding: 10, backgroundColor: "white" }}>
+                    <View>
+                        <Image source={{ uri: "https://i.pinimg.com/736x/23/a2/f3/23a2f39014f3db9981ba006a536deb3b.jpg" }} style={{ width: 190, height: 160, borderRadius: 10, opacity:0.6 }} />
+                        <Text style={{ padding: 7, position: "absolute", top: 20, left: 55, color: "black", fontSize: 20, fontWeight: "bold" }}>Sermons</Text>
+                    </View>
+                    <View>
+                        <Image source={{ uri: "https://i.pinimg.com/736x/23/a2/f3/23a2f39014f3db9981ba006a536deb3b.jpg" }} style={{ width: 190, height: 160, borderRadius: 10, opacity:0.6 }} />
+                        <Text style={{ padding: 7, position: "absolute", top: 20, left: 35, color: "black", fontSize: 20, fontWeight: "bold" }}>Get connected</Text>
+                    </View>
+
+                </View>
+
+                <Text style={{ fontSize: 25, paddingHorizontal: 5, fontWeight: "bold", marginVertical:10 }}>Upcoming Events</Text>
+
+                <View>
+                    <View style={{marginVertical: 10}}>
+                        <Image source={{ uri: "https://t4.ftcdn.net/jpg/03/39/68/93/360_F_339689377_G6x4Hmjo9lmGUEytSBbU5cS3lEOkgU9m.jpg" }} style={{ height: 200, borderRadius: 10, opacity:0.6 }} />
+                        <Text style={{ padding: 7, position: "absolute", top: 30, left: 60, color: "black", fontSize: 30, fontWeight: "bold" }}>WOMEN'S MEETING</Text>
+                    </View>
+
+                    <View style={{marginVertical: 10}}>
+                        <Image source={{ uri: "https://t4.ftcdn.net/jpg/03/39/68/93/360_F_339689377_G6x4Hmjo9lmGUEytSBbU5cS3lEOkgU9m.jpg" }} style={{ height: 200, borderRadius: 10, opacity:0.6 }} />
+                        <Text style={{ padding: 7, position: "absolute", top: 30, left: 60, color: "black", fontSize: 30, fontWeight: "bold" }}>BIBLE STUDY NIGHT</Text>
+                    </View>
+                </View>
+
+
+
+
             </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
--- Screens/More.js
+++ Screens/More.js
@@ -1,12 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export function More() {
   return (
-    <View>
-      <Text>More</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Image source={{ uri: 'https://xsgames.co/randomusers/assets/images/favicon.png' }}
+          style={{ width: 180, height: 180, alignSelf: "center" }} />
+        <Text style={{ fontWeight: "bold", fontSize: 25, textAlign: "center" }}>Jane Doe</Text>
+        <Text style={{ textAlign: "center", fontSize: 20, marginBottom: 20 }}>Janedoe@gmail.com</Text>
+
+        <View style={{ margin: 40, borderRadius: 10, padding: 20, elevation: 5 }}>
+          <Text style={{ fontWeight: "bold", fontSize: 25, }}>Options</Text>
+
+          <View style={{ flexDirection: "row", gap: 30, padding: 20 }}>
+            <Ionicons name="notifications" size={24} color="black" />
+            <Text style={{ fontSize: 20 }}>Notifications</Text>
+          </View>
+
+          <Text style={{ fontSize: 25, fontWeight: "bold" }}>Support</Text>
+
+          <View style={{ flexDirection: "row", gap: 30, padding: 20 }}>
+          <MaterialIcons name="contact-support" size={24} color="black" />
+            <Text style={{ fontSize: 20 }}>FAQS</Text>
+          </View>
+
+          <View style={{ flexDirection: "row", gap: 30, padding: 20 }}>
+          <MaterialIcons name="support-agent" size={24} color="black" />
+            <Text style={{ fontSize: 20 }}>Help</Text>
+          </View>
+
+          <Text style={{fontSize:20, textAlign:"center", marginVertical: 10}}>Log out</Text>
+
+        </View>
+
+      </View>
+    </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  }
+})
