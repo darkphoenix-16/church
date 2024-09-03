6b211511e29eaf7510732a6cce83b89f4233ca6a
oyindamola adeonigbagbe <oyindamolatabitha@gmail.com>
2024-09-03 15:19:20 +0100

    commit

--- Screens/Connect.js
+++ Screens/Connect.js
@@ -1,12 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { MaterialIcons } from '@expo/vector-icons'
 
 export function Connect() {
   return (
-    <View>
-      <Text>Profile</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+    <View style={styles.container}>
+      <TouchableOpacity style={{flexDirection:"row", justifyContent:"space-between", alignItems:"center", backgroundColor:"white", elevation:10, padding:25, borderRadius:15,}}>
+        <Text style={{fontSize:25 , fontWeight:"condensedBold"}}>Become a member</Text>
+        <MaterialIcons name="keyboard-arrow-right" size={24} color="black" />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={{flexDirection:"row", justifyContent:"space-between", alignItems:"center", backgroundColor:"white", elevation:10, padding:25, borderRadius:15, marginVertical:20}}>
+        <Text style={{fontSize:25 , fontWeight:"condensedBold"}}>Join a group</Text>
+        <MaterialIcons name="keyboard-arrow-right" size={24} color="black" />
+      </TouchableOpacity>
+
     </View>
-  )
+  </SafeAreaView>
+)
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  padding: 10,
+  backgroundColor: "antiquewhite",
+  justifyContent:"center"
+
+}
+})
--- Screens/Media.js
+++ Screens/Media.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { MaterialIcons } from '@expo/vector-icons'
@@ -7,10 +7,15 @@ export function Media() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
-        <View>
-          <Text>Sermons</Text>
+        <TouchableOpacity style={{flexDirection:"row", justifyContent:"space-between", alignItems:"center", backgroundColor:"white", elevation:10, padding:25, borderRadius:15,}}>
+          <Text style={{fontSize:25 , fontWeight:"condensedBold"}}>Sermons</Text>
           <MaterialIcons name="keyboard-arrow-right" size={24} color="black" />
-        </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={{flexDirection:"row", justifyContent:"space-between", alignItems:"center", backgroundColor:"white", elevation:10, padding:25, borderRadius:15, marginVertical:20}}>
+          <Text style={{fontSize:25 , fontWeight:"condensedBold"}}>LiveStreams</Text>
+          <MaterialIcons name="keyboard-arrow-right" size={24} color="black" />
+        </TouchableOpacity>
 
       </View>
     </SafeAreaView>
@@ -22,6 +27,7 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     backgroundColor: "antiquewhite",
+    justifyContent:"center"
 
   }
 })
